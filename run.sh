83b721cd072f6a8384943be241aacd76d89edac9
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-12-09 21:13:38 -0800

    Add run script for Docker container setup

---
+++ run.sh
@@ -0,0 +1,5 @@
+docker -d \
+-v /usr/share/MTV/mtv.db:/usr/share/MTV/mtv.db \
+-v /usr/share/MTV/thumbnails/:/usr/share/MTV/thumbnails/ \
+-v /usr/share/MTV/Posters/:/usr/share/MTV/Posters/ \
+-p 8082:80 .
