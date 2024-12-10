6c961eb45f3d4aaa72a200c7cb45f19138d93923
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-12-09 21:16:33 -0800

    Update run script to build Docker image and run in detached mode

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-docker -d \
+docker build -t mtvtserver:latest \
+-d \
 -v /usr/share/MTV/mtv.db:/usr/share/MTV/mtv.db \
 -v /usr/share/MTV/thumbnails/:/usr/share/MTV/thumbnails/ \
 -v /usr/share/MTV/Posters/:/usr/share/MTV/Posters/ \
