3dfadfff47719d1e4d7d80547e31e3353f5728cf
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2025-03-03 02:01:15 -0800

    Add X-Men page with data fetching; update WASM files and styles

--- src/movxpages.rs
+++ src/movxpages.rs
@@ -1,8 +1,52 @@
-use leptos::prelude::*;
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+use leptos::{prelude::*, task::spawn_local};
+use reqwest::Error;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug, Clone)]
+struct XMen {
+    Name: String,
+    Year: String,
+    PosterAddr: String,
+    Size: String,
+    Path: String,
+    Idx: String,
+    MovId: String,
+    Catagory: String,
+    HttpThumbPath: String,
+}
 
 #[component]
 pub fn XMenPage() -> impl IntoView {
+    let (xmen, set_xmen) = signal(Vec::new());
+
+    spawn_local(async move {
+        match fetch_xmen().await {
+            Ok(data) => {
+                log::info!("Fetched X-Men data: {:?}", data); // Debugging log
+                println!("{:?}", data); // Debugging log
+                set_xmen.set(data);
+            },
+            Err(err) => log::error!("Error fetching X-Men data: {:?}", err),
+        }
+    });
+
     view! {
-        <h1>"X Men Page"</h1>
+        <div class="xmen-grid">
+            {move || xmen.get().iter().map(|xman| view! {
+                <div class="xmen-item">
+                    <img src={xman.HttpThumbPath.clone()} alt={xman.Name.clone()} />
+                    <p>{xman.Name.clone()}</p>
+                </div>
+            }).collect_view()}
+        </div>
     }
+}
+
+async fn fetch_xmen() -> Result<Vec<XMen>, Error> {
+    let response = reqwest::get("http://10.0.4.41:7777/xmen").await?;
+    let xmen: Vec<XMen> = response.json().await?;
+    println!("{:?}", xmen);
+    Ok(xmen)
 }
